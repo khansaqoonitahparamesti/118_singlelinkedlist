cf25ce89e93e80a8aa4d1c12d32f409943674884
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-04-26 11:16:47 +0700

    membuat dan menambah kan node baru

--- 118_singlelinkedlist/118_singlelinkedlist.cpp
+++ 118_singlelinkedlist/118_singlelinkedlist.cpp
@@ -12,3 +12,14 @@ struct node {
 
 node* START = NULL;
 
+void addnode() {
+	int nim;
+	string nama;
+	node* nodebaru = new node;
+	cout << " masukkan NIM:";
+	cin >> nim;
+	cout << "masukkan Nama";
+	cin >> nama;
+	nodebaru->noMhs = nim;
+	nodebaru->name = nama;
+}
